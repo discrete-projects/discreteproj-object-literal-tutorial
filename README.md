7a30e69da9ccbfe05326f199706679951defd31f
sansimian <aterruso@gmail.com>
2016-10-30 18:14:57 -0400

    Updated README | App now adds and removes list items. | CSS cleanup.

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# RULES
+# Modular JS Using Object Literal Pattern
+## Ground Rules
 1. Self-contained module
 	- All code is broken out into modules that handle specific tasks
 	- No Global Variables
 	- If a module manages more than one thing it is split up into a different module
-2. Seperation of Concers
+2. Separation of Concerns
 3. DRY Code - Do Not Repeat Yourself
-4 Efficient DOM usage
+4. Efficient DOM usage
 	- Very few $(selectors)
 5. No memory leaks
 	- All events can be unbound
 
-
+## Objective
+We will create a very basic app that adds and removes names to a list. The app is going to be dumb and have no db but that's ok because right now we just want to focus on how we organize code using the Object Literal Pattern.
 
 ## Object Literal Pattern
-Object Literals provide a very convenient notation for creation new object values. An obect literal is a pair of curly braces surroundzero or more name/value pairs. An object literal can appear anywehere an expression can appear:
+Object Literals provide a very convenient notation for the creation of new object values. An object literal is a pair of curly braces surround by zero or more name/value pairs. An object literal can appear anywhere an expression can appear:
 
-`var empty_object=();
+`var empty_object=();<br/><br/>
 
-var stooge = {
-	"first-name": "Jerome",
-	"last-name": "Howard"
+var stooge = {<br/>
+	"first-name": "Jerome",<br/>
+	"last-name": "Howard"<br/>
 };
 `
 
 You can also create a Method or a function nested within an Object Literal:
-` var humans = {
-	name: 'Anthony',
-	age: 30,
-	sayName: function(){
-	alert(this.name);
+` var humans = {<br/>
+	name: 'Anthony',<br/>
+	age: 30,<br/>
+	sayName: function(){<br/>
+	alert(this.name);<br/>
 }
 };
 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,48 @@
 console.log('sup world');
 
-// var people = [];
-// var template = $('#people-template').html();
+(function() {
+  var people = {
+    people: ['Will', 'Steve'],
+    init: function() {
+      this.cacheDom();
+      this.bindEvents();
+      this.render();
+    },
+    cacheDom: function() {
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template = this.$el.find('#people-template').html();
+    },
+    bindEvents: function() {
+      this.$button.on('click', this.addPerson.bind(this));
+      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
+    },
+    render: function() {
+      Mustache.render('hi {{name}}', {
+        name: 'Will'
+      }); //hi will
+      var data = {
+        people: this.people,
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    addPerson: function() {
+      console.log('add');
+      this.people.push(this.$input.val());
+      this.render();
+      this.$input.val(' ');
+    },
+    deletePerson: function(e) {
+      console.log('delete');
+      var $remove = $(e.taget).closest('li');
+      var i = this.$ul.find('li').index($remove);
+      this.people.splice(i, 1);
+      this.render();
+    }
+  };
 
-// $('#peopleModule').find('button').on('click', function(){
-// 	people.push($('#peopleModule').find('input').val());
-// 	$('#peopleModule').find('input').val('');
-// 	var data = {people:people,
-// 	};
-// 	$('#peopleModule').find('ul').html(Mustache.render(template, data));
-// });
+  people.init();
 
-// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e){
-// 	var $remove = $(e.target).closest('li');
-// 	var i = $('#peopleModule').find('ul').find('li').index($remove);
-// 	$remove.remove();
-// 	people.splice(i, 1);
-// })
-
-
- var humans = {
-	name: 'Anthony',
-	age: 30,
-	sayName: function(){
-	alert(this.name);
-}, 
-setName: function(newName){
-	this.name = newName;
-}
-};
-
-humans.setName('Willis');
-humans.sayName();
+})();
